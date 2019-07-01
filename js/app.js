7f5f8d9d835eeb9bf1e923381e123d19c4dd0ad1
faywinne <faywinne@users.noreply.github.com>
2019-07-01 09:05:47 -0500

    drop term element
    instead of manipulating innerHTML, uses appendChild to move the whole element

--- js/app.js
+++ js/app.js
@@ -11,16 +11,20 @@ $.ready(function() {
 	var dropArea = document.getElementById("definitionWidget1");
 
 	function startDragItemFunc(e) {
+		//e.target is dragged item
 		console.log("drag start:" + e.target.id);
 		e.dataTransfer.setData("text", e.target.id);
 	}
 
 	function dropItemFunc(e) {
+		//e.target is receiving item
 		e.preventDefault();
+		console.log("drag end on:" + e.target.id);
 		var data = e.dataTransfer.getData("text");
-		var dragEl = document.getElementById(data)
-		e.target.innerHTML = dragEl.innerHTML;
-		dragEl.innerHTML = "";
+		var dragEl = document.getElementById(data);
+		//e.target.innerHTML = dragEl.innerHTML;
+		//dragEl.innerHTML = "";
+		e.target.appendChild(dragEl);
 	}
 
 	function dragOverItemFunc(e) {
